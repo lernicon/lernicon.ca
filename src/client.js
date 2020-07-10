1987cffd6684f5a43224d468fa6bf29b14b8fb44
Lernicon Academy Inc <admin@lernicon.ca>
2020-07-10 11:55:24 -0700

    fix bundler config

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-import App from "./App.svelte";
+import App from './app/App.svelte';
 
 var app = new App({
   target: document.getElementById('app'),
@@ -9,9 +9,11 @@ export default app;
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
+/*
 if (import.meta.hot) {
   import.meta.hot.accept();
   import.meta.hot.dispose(() => {
     app.$destroy();
   });
 }
+*/
